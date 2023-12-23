0a63dc61c55a1f02829f23ea7c81ab0c52bde275
hamidreza Hosseinpour <hamidrezaonlyh@gmail.com>
2023-12-23 11:03:29 +0330

    new changes

---
+++ js/game.js
@@ -0,0 +1,20 @@
+const loader = document.getElementById("loader");
+const container = document.getElementById("container");
+
+const URL =
+  "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
+let formattedData = null;
+
+const fetchData = async () => {
+  const response = await fetch(URL);
+  const json = await response.json();
+  formattedData = json;
+  start();
+};
+
+const start = () => {
+  loader.style.display = "none";
+  container.style.display = "block";
+};
+
+window.addEventListener("load", fetchData);
